c0a400579c8c665cbb95248ac0ad2d2ba56125a7
int-code <pubalibasak.cse2020@nsec.ac.in>
2023-03-25 17:04:00 +0530

    initial commit

---
+++ credito/chat.py
@@ -0,0 +1,64 @@
+import openai
+import re
+import os
+def test():
+    return "hello"
+
+class chatbot:
+    def __init__(self) -> None:
+        openai.api_key=os.getenv("api_key")
+        self.questionBank = []
+
+    def getAns(self,credit_score: int, num_cards: int, question: str) -> str:
+        personal_info = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=[
+                {"role":"system", "content":"You are a helpful assistant. Give very short definitive yes or no answers"},
+                {"role":"user", "content":"Does the question '"+question+"' require personal information?"}
+            ],
+            temperature=0.2
+        )['choices'][0]['message']['content']
+
+        if(personal_info=="Yes."):
+            # print("Personal info detected. resetted")
+            message=[
+                    {"role": "system", "content": "You are a helpful assistant who gives good credit advice.Give short definitive answers.My credit score is "+str(credit_score)+" and I have "+str(num_cards)+"credit cards."},
+                    {"role": "user", "content": question}
+                ]
+            response =openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=message
+            )
+            message.append(response['choices'][0]['message'])
+            self.questionBank=[message]
+
+        elif re.match(r'^no',personal_info, re.IGNORECASE):
+            # print("Personal info NOT detected")
+            if len(self.questionBank)==0:
+                message=[{"role": "system", "content": "You are a helpful assistant who gives good credit advice.Give short definitive answers.My credit score is "+str(credit_score)+" and I have "+str(num_cards)+"credit cards."}]
+            else:
+                message=self.questionBank[0]
+            message.append({"role": "user", "content": question})
+            # print(message)
+            response =openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=message
+            )
+            message.append({"role":"assistant", "content":response['choices'][0]['message']['content']})
+            self.questionBank.append(message)
+        else:
+            return personal_info
+        return response['choices'][0]['message']['content']
+
+    def getSuggestions(topic: str) -> list[str]:
+        response =openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role":"system", "content":"Pick 3 questions from the given list which relate the most to the given topic-\nCheck my credit score\nWhat is credit score?\nWhat are credit cards?\nHow to improve my score?\nRequirements for getting a credit card?\nHow to increase credit limit?"},
+                {"role":"user", "content":"credit card"},
+                {"role":"assistant", "content":"What are credit cards?,Requirements for getting a credit card?,How to increase credit limit?"},
+                {"role": "user", "content":topic}
+            ]
+            )
+        return response['choices'][0]['message']['content'].split(", ")
+    
